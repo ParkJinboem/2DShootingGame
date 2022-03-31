0beac93d286df7bb9fe8585d9a53ec00c13ffe67
ParkJinboem <101230664+ParkJinboem@users.noreply.github.com>
2022-03-31 14:34:14 +0900

    Revert "Revert "골드 추가""
    This reverts commit 832cf8ed5937dfa55fcc1f57e5a07032ab19e8fb.

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject startPos;
     public GameObject Enemy;
 
+    public static GameManager instance;
+    public Text textGold;
+    public int Gold;
+    public int tempGold;
+
     private void Start()
     {
-        StartCoroutine("EnemySpawn");       
+        instance = this;
+        Gold = 0;
+        StartCoroutine("EnemySpawn");
+    }
+    private void Update()
+    {
+        if(Gold != tempGold)
+        {
+            tempGold = Gold;
+            textGold.text = Gold.ToString();
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Gold += 10;
+            textGold.text = Gold.ToString();
+        }
     }
     IEnumerator EnemySpawn()
     {
-        Instantiate(Enemy, startPos.transform.position + new Vector3(1,4,-1), Quaternion.identity);
+        Instantiate(Enemy, startPos.transform.position + new Vector3(1,2,-1), Quaternion.identity);
         yield return new WaitForSeconds(5.0f);
         StartCoroutine("EnemySpawn");
 
